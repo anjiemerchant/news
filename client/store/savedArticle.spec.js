893379f5a3c32d37ceb088dc472f932513568517
manja <anjiemerchant@gmail.com>
2018-05-03 08:02:10 -0400

    one more test on saved article spec

--- client/store/savedArticle.spec.js
+++ client/store/savedArticle.spec.js
@@ -1,16 +1,16 @@
 import {expect} from 'chai';
 import savedArticleReducer, {getAllSavedArticles, addNewArticle, GET_ALL_SAVED_ARTICLES, ADD_NEW_ARTICLE} from './savedArticles';
 import {createStore} from 'redux';
-import {range} from 'lodash';
+import {range, last} from 'lodash';
 
 const createRandomArticles = amount => {
   return range(0, amount).map(index => {
       return {
           id: index + 1,
           title: `test + ${index + 1}`,
-      };
-  });
-};
+      }
+  })
+}
 
 describe('savedArticle redux architecture', () => {
   describe('action creators', () => {
@@ -56,21 +56,21 @@ describe('savedArticle reducer', () => {
           existingRandomArticles = createRandomArticles(5);
           testingStore = createStore(
               savedArticleReducer,
-              {savedArticles: existingRandomArticles}
+              existingRandomArticles
           );
       });
 
-      // it('affects the state by appending a new article to state saved articles', () => {
-      //     const newArticle = testUtilities.createOneRandomArticle();
-      //     testingStore.dispatch({
-      //         type: ADD_NEW_ARTICLE,
-      //         article: newArticle
-      //     });
-      //     const newState = testingStore.getState()
-      //     const lastArticleOnState = last(newState.savedArticles);
-      //     expect(newState.savedArticles).to.have.length(6);
-      //     expect(lastArticleOnState).to.be.deep.equal(newArticle);
-      // });
+      it('affects the state by appending a new article to state saved articles', () => {
+          const newArticle = createRandomArticles(1);
+          testingStore.dispatch({
+              type: ADD_NEW_ARTICLE,
+              article: newArticle
+          });
+          const newState = testingStore.getState()
+          const lastArticleOnState = last(newState);
+          expect(newState).to.have.length(6);
+          expect(lastArticleOnState).to.be.deep.equal(newArticle);
+      });
 
       it('sets savedArticles to different array from previous state', () => {
           const originalState = testingStore.getState();
@@ -80,8 +80,8 @@ describe('savedArticle reducer', () => {
             article: newArticle
           });
           const newState = testingStore.getState();
-          expect(newState.savedArticles).to.not.be.equal(originalState.savedArticles);
-          expect(originalState.savedArticles).to.have.length(5);
+          expect(newState).to.not.be.equal(originalState);
+          expect(originalState).to.have.length(5);
       });
   });
 });
